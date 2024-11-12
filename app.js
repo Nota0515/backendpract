b26b5a2603dfa3b9cfd09097f54bc2c848801723
Nota0515 <businessbro78@gmail.com>
2024-11-12 19:43:15 +0530

    created login and register

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRoute = require('./routes/user.routes')
 const app = express();
+const cookieParser = require('cookie-parser')
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -8,6 +9,7 @@ const connectToDB = require('./config/db')
 connectToDB();
 
 app.set('view engine', 'ejs')
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { body,validationResult } = require('express-validator');
 const userModel = require('../models/user.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 router.get('/register',
@@ -12,9 +14,9 @@ router.get('/register',
 
 router.post('/register' , 
     body('email').trim().isEmail().isLength({min:10}),
-    body('passwords').trim().isLength({min:6}),
+    body('password').trim().isLength({min:3}),
     body('username').trim().isLength({min:3}),
-    (req,res)=>{
+   async (req,res)=>{
         const error = validationResult(req)
 
         if(!error.isEmpty()){
@@ -23,9 +25,70 @@ router.post('/register' ,
                 message: 'Invalid data'
             })
         }
-     
-        res.send(error)
+
+        const {username , email , password} = req.body;
+        const haspassword = await bcrypt.hash(password , 10)
+        const newUser = await userModel.create({
+            username,
+            email,
+            password: haspassword
+        })
+
+        res.json(newUser)
     }
 )
+
+router.get('/login' , (req,res)=>{
+    res.render('login')
+})
+
+router.post('/login', 
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:3}),
+    async (req , res)=>{
+        const error = validationResult(req.body)
+
+        if(!error.isEmpty()){
+            return res.status(400).json({
+                error: error.array(),
+                message: 'Invalid Data'
+            })
+        }
+
+        const {username , password} = req.body;
+        const user = await userModel.findOne({
+            username: username
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message:'Username or password incorrect'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            res.status(400).json({
+                message: "invalid username or password"
+            })
+        }
+
+        const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+    )
+
+    res.cookie('token' , token)
+    res.send('logedIn')
+
+
+    }
+)
+
+
 
 module.exports = router;
